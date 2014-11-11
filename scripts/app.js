d42cdadd886a3bb1130736b8a2c9a4e5dc5391e3
sanjay-ak-us <sanjay.ak.us@gmail.com>
2014-11-11 18:18:49 +0100

    getNow changed

--- scripts/app.js
+++ scripts/app.js
@@ -4,20 +4,20 @@ var url = "http://echo.jsontest.com/hello/world";
 var getNow = function(){
 	//var result = moment().format('YYYY-MM-DD | HH.MM.ss.SS');
 	var result = new Date();
-	var YYYY = result.getFullYear();
+	/*var YYYY = result.getFullYear();
 	var MM = result.getMonth();
 	var DD = result.getDay();
 	var HH = result.getHours();
 	var MM = result.getMinutes();
 	var ss = result.getSeconds();
 	var SS = result.getMilliseconds(); 
-	YYYY+'-'+MM+'-'+DD+' | '+HH+'.'+MM+'.'+ss+'.'+SS;
+	YYYY+'-'+MM+'-'+DD+' | '+HH+'.'+MM+'.'+ss+'.'+SS;*/
 	return result.toLocaleString();
 };
 
 var localClock = function(){
 	$('.local-time').text(getNow());
-	setTimeout(function(){localClock();},500);
+	setTimeout(localClock,500);
 };
 
 function asyncDemo(url){
